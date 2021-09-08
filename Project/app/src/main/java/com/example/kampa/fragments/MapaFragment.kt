acad6f4e50c62c40af4792111c8e6136761ad2c3
Mariana <a01702593@itesm.mx>
2021-09-08 09:16:10 -0500

    Mapa

--- Project/app/src/main/java/com/example/kampa/fragments/MapaFragment.kt
+++ Project/app/src/main/java/com/example/kampa/fragments/MapaFragment.kt
@@ -28,6 +28,7 @@ class MapaFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
