cae29fe107bd692ef137d1ae395faaa8a1ee8135
RafaelHinojosa <rafael.zeo@gmail.com>
2021-09-22 17:15:37 -0500

    Archivo de Constantes

---
+++ Project/app/src/main/java/com/example/kampa/Constantes.kt
@@ -0,0 +1,26 @@
+package com.example.kampa
+
+class Constantes {
+
+    val DESCRIPCION = "descripcion"
+    val DIRECCION = "direccion"
+    val ESTADO = "estado"
+    val FOTO = "foto"
+    val FOTOS = "fotos"
+    val FOTO_PERFIL = "fotoPerfil"
+    val ID_PUBLICACION = "idPublicacion"
+    val ID_ROL = "idRol"
+    val ID_SITIO = "idSitio"
+    val ID_TAG = "idTag"
+    val ID_TIPO_SITIO = "idTipoSitio"
+    val ID_USUARIO = "idUsuario"
+    val ID_WISHLIST = "idWishlist"
+    val IS_VISITADO = "isVisitado"
+    val IS_WISHLIST = "isWishlist"
+    val NOMBRE = "nombre"
+    val NUM_DISLIKES = "numDislikes"
+    val NUM_LIKES = "numLikes"
+    val NUM_PUBLICACIONES = "numPublicaciones"
+    val UBICACION = "ubicacion"
+
+}
