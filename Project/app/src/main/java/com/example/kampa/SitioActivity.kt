d10555d3ceb4c572f72ab3d112771489574998aa
Mariana <a01702593@itesm.mx>
2021-09-22 12:11:55 -0500

    Iniciar carousel

---
+++ Project/app/src/main/java/com/example/kampa/SitioActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kampa
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import com.denzcoskun.imageslider.ImageSlider
+//import com.denzcoskun.imageslider.models.SlideModel
+
+
+class SitioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sitio)
+
+//        val imageSlider : ImageSlider = findViewById(R.id.slider)
+//        val slideModels : List<SlideModel> = listOf(SlideModel(R.drawable.arcos1), SlideModel(R.drawable.arcos2), SlideModel(R.drawable.arcos3), SlideModel(R.drawable.arcos2))
+//
+//        imageSlider.setImageList(slideModels, true)
+
+    }
+
+
+
+}
