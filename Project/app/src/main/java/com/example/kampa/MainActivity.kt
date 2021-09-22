46d2f6e8cb186218b2e7180905fcfea3d27901c6
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2021-09-22 12:59:16 -0500

    Crear card view para las imagenes de descrubre

--- Project/app/src/main/java/com/example/kampa/MainActivity.kt
+++ Project/app/src/main/java/com/example/kampa/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.kampa
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.kampa.fragments.DescubreFragment
 import com.example.kampa.fragments.FavoritosFragment
@@ -12,12 +13,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
 
     // Variables para navegación en el Menú
     private val descubreFragment = DescubreFragment()
     private val mapaFragment = MapaFragment()
     private val favoritosFragment = FavoritosFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,24 @@ class MainActivity : AppCompatActivity() {
 
         // Crear listener de Menu Nav para intercambiar fragmentos
         val menuNav = findViewById<BottomNavigationView>( R.id.menuNav)
+
+
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_enabled)
+        )
+
+        val colors = intArrayOf(
+            Color.parseColor("#D83670"),
+            Color.parseColor("#15CDCD"),
+            Color.parseColor("#ECB518")
+        )
+
+
+
+        val myList = ColorStateList(states, colors)
+        //menuNav.menu.findItem(R.id.iconoDescubre).iconTintList
+        //menuNav.itemIconTintList = myList
+
         menuNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.iconoDescubre -> replaceFragment(descubreFragment)
