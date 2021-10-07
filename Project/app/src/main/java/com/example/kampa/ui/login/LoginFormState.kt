282b30114a8af37132a4fe7c39ebd5f3fff486bc
Joaquinzer <A01610399@itesm.mx>
2021-10-07 18:28:52 -0500

    Login y sign up

--- Project/app/src/main/java/com/example/kampa/ui/login/LoginFormState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kampa.ui.login
-
-/**
- * Data validation state of the login form.
- */
-data class LoginFormState(
-    val passwordError: Int? = null,
-    val isDataValid: Boolean = false
-)
--- Project/app/src/main/java/com/example/kampa/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kampa.ui.login
-
-/**
- * Authentication result
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
