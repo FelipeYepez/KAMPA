5e2730a1828c9346f293c7811b8636eec23c6762
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2021-10-12 19:51:45 -0500

    Visualizar denuncias basado en su estado

---
+++ Project/app/src/main/java/com/example/kampa/adapters/DenunciasAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.kampa.adapters
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kampa.R
+import com.example.kampa.models.Denuncia
+import com.parse.GetDataCallback
+import com.parse.ParseFile
+
+class DenunciasAdapter(private val context: Context, private val data: ArrayList<Denuncia>): RecyclerView.Adapter<DenunciasAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DenunciasAdapter.ViewHolder {
+        var layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_denuncias, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var item: Denuncia = data[position]
+        holder.bind(item, context)
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private var fotoDenuncia: ImageView = view.findViewById(R.id.fotoDenuncia)
+        private var denunciasTitle: TextView = view.findViewById(R.id.denunciasTitle)
+        private var denunciaDescripcion: TextView = view.findViewById(R.id.denunciaDescripcion)
+
+        private fun loadImages(foto: ParseFile?, imgView: ImageView) {
+            if (foto != null) {
+                foto.getDataInBackground(GetDataCallback { data, e ->
+                    if (e == null) {
+                        val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+                        imgView.setImageBitmap(bmp)
+                    }
+                    else {
+                        imgView.setImageResource(R.drawable.esencia_patrimonio)
+                    }
+                })
+
+            }
+        }
+
+        fun bind(item: Denuncia, context: Context) {
+            loadImages(item.fotos, fotoDenuncia)
+            denunciasTitle.setText(item.idSitio?.nombre)
+            denunciaDescripcion.setText(item.descripcion)
+        }
+
+    }
+
+}
