20b63ea2969d25a12f37a4d05ff9986332b7eca0
Mariana <a01702593@itesm.mx>
2021-10-18 13:42:37 -0500

    JavaDoc MapaFragment y NuevoSitioActivity

---
+++ Project/app/src/test/java/com/example/kampa/NuevoSitioActivityTest.kt
@@ -0,0 +1,9 @@
+package com.example.kampa
+
+import com.example.kampa.fragments.MapaFragmentUtils
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class NuevoSitioActivityTest {
+
+}
