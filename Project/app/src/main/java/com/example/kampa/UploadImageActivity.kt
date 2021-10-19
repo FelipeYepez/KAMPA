a75f97e0614cd99fa58b9477f35adf7f27b303ea
JARE2001 <A01701350@itesm.mx>
2021-10-18 23:40:30 -0500

    push

--- Project/app/src/main/java/com/example/kampa/UploadImageActivity.kt
+++ Project/app/src/main/java/com/example/kampa/UploadImageActivity.kt
@@ -22,6 +22,7 @@ import java.util.concurrent.ExecutorService
 
 typealias LumaListener = (luma: Double) -> Unit
 
+//hola
 class UploadImageActivity : AppCompatActivity() {
 
     var imageCapture: ImageCapture? = null
